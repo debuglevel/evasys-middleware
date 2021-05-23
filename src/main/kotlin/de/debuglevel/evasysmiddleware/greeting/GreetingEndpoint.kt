2068d45a192eeb7031a835c5a42addea3f4cc1be
Marc Kohaupt <debuglevel@gmail.com>
2021-05-23 22:30:10 +0200

    Disable gRPC

--- src/main/kotlin/de/debuglevel/evasysmiddleware/greeting/GreetingEndpoint.kt
+++ src/main/kotlin/de/debuglevel/evasysmiddleware/greeting/GreetingEndpoint.kt
@@ -1,19 +1,19 @@
-package de.debuglevel.evasysmiddleware.greeting
-
-import mu.KotlinLogging
-import javax.inject.Singleton
-
-@Singleton
-class GreetingEndpoint(
-    private val greetingService: GreetingService
-) : GreetingGrpcKt.GreetingCoroutineImplBase() {
-    private val logger = KotlinLogging.logger {}
-
-    override suspend fun greet(request: GreetRequest): GreetReply {
-        logger.debug("Called greet($request)")
-
-        val greeting = greetingService.greet(request.name, request.locale)
-        val greetReply = GreetReply.newBuilder().setMessage(greeting.greeting).build()
-        return greetReply
-    }
-}
+//package de.debuglevel.evasysmiddleware.greeting
+//
+//import mu.KotlinLogging
+//import javax.inject.Singleton
+//
+//@Singleton
+//class GreetingEndpoint(
+//    private val greetingService: GreetingService
+//) : GreetingGrpcKt.GreetingCoroutineImplBase() {
+//    private val logger = KotlinLogging.logger {}
+//
+//    override suspend fun greet(request: GreetRequest): GreetReply {
+//        logger.debug("Called greet($request)")
+//
+//        val greeting = greetingService.greet(request.name, request.locale)
+//        val greetReply = GreetReply.newBuilder().setMessage(greeting.greeting).build()
+//        return greetReply
+//    }
+//}
